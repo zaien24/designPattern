f827923e58200c276215a80a74bc427745bbc649
DESKTOP-9CTRFPU\zaien24 <zaien24@gmail.com>
2022-07-16 02:05:17 +0900

    클래스의 약한 결합, 강한 결합의 이해1

--- src/UnderStandDI.java
+++ src/UnderStandDI.java
@@ -1,9 +1,35 @@
+import java.util.Date;
 
 public class UnderStandDI {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//강한 결합
+		Date date = new Date();
+		System.out.println(date);
+	}
+	
+	public static void getDate(Date d) {
+		//약한 결합
+		Date date = d;
+		System.out.println(date);
+	}
+	
+	public static void memberUse1() {
+		//강한 결합 : 직접 생성
+		Member m1 = new Member();
+	}
+	
+	public static void memberUse2(Member m) {
+		// 약한 결합 : 생성된 것을 주입 받음 - 의존 주입 (Dependency Injection)
 	}
 
 }
+
+// Member를 사용한다 --> Member 의 기능에 의존한다 라는 의미 
+class Member {
+	String name;
+	String nickname;
+	public Member() {}
+}
+
+
