8f4c86830efc858e700add7a73a3be0f2ed0daf9
DESKTOP-9CTRFPU\zaien24 <zaien24@gmail.com>
2022-07-18 01:56:57 +0900

    실전: 로그라이터 만들기 2 동시에 많은 쓰레드 호출

---
+++ src/logwriter/case2/TestPattern2.java
@@ -0,0 +1,30 @@
+package logwriter.case2;
+
+public class TestPattern2 {
+
+	public static void main(String[] args) {		
+		for (int i = 0; i < 50; i++) {
+			Thread t = new ThreadSub(i);
+			t.start();
+		}
+	}
+}
+
+class ThreadSub extends Thread {
+	int num;
+	
+	public ThreadSub(int num) {
+		this.num = num;
+	}
+	
+	@Override
+	public void run() {
+		LogWriter logger = LogWriter.getInstance();
+		if (num < 10)
+			logger.log("*** 0 " + num + "***");
+		else 
+			logger.log("*** " + num + " ***");
+	}
+}
+
+
