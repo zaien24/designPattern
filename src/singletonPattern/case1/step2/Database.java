5d89e291091f148a2af9871105e3f5d637a7c7ee
DESKTOP-9CTRFPU\zaien24 <zaien24@gmail.com>
2022-07-18 01:20:03 +0900

    생성자 문제점과 해결 그리고 쓰레드 사용시 문제점 파악하기 문제처리 1

---
+++ src/singletonPattern/case1/step2/Database.java
@@ -0,0 +1,31 @@
+package singletonPattern.case1.step2;
+
+public class Database {
+	private static Database singleton;
+	private String name;
+
+	private Database(String name) {
+//		super();
+//		this.name = name;
+		
+		try {
+			Thread.sleep(100);
+			this.name = name;
+		} catch (Exception e) {
+			
+		}
+	}
+	
+	public synchronized static Database getInstance(String name) {
+		if (singleton == null) {
+			singleton = new Database(name);
+		}
+		return singleton;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	 
+}
