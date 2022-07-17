71aa3be29b5ef562b5d29d1265eab130ca4be712
DESKTOP-9CTRFPU\zaien24 <zaien24@gmail.com>
2022-07-18 00:26:52 +0900

    객체의 생성 개수 제한하기 1

---
+++ src/singletonPattern/case1/step1/Database.java
@@ -0,0 +1,16 @@
+package singletonPattern.case1.step1;
+
+public class Database {
+	private String name;
+
+	public Database(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	 
+}
