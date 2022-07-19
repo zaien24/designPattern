555bc4252350331ff0d1ae6b6c2c999d9c1ee86c
DESKTOP-9CTRFPU\zaien24 <zaien24@gmail.com>
2022-07-20 00:55:43 +0900

    Flyweight pattern 기존 자바 코드에서 볼 수 있는 플라이웨이트 패턴

---
+++ src/flyweightPattern/case1/step1/TestPattern.java
@@ -0,0 +1,15 @@
+package flyweightPattern.case1.step1;
+
+public class TestPattern {
+
+	public static void main(String[] args) {
+		String str1 = new String("홍길동");
+		String str2 = new String("홍길동");
+		String str3 = "홍길동";
+		String str4 = "홍길동";
+		
+		System.out.println("Flyweight Pattern");
+
+	}
+
+}
