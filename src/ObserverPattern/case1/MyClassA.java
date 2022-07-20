a2e5a70ff807dab0e298183897fdc00a4a0b11c4
DESKTOP-9CTRFPU\zaien24 <zaien24@gmail.com>
2022-07-20 20:53:29 +0900

    Observer Pattern Java 에서 제공하는 기능을 이용한 옵저버 패턴 만들기

--- src/ObserverPattern/case1/MyClassA.java
+++ src/ObserverPattern/case1/MyClassA.java
@@ -1,35 +1,32 @@
-package ObserverPattern.case1;
+package case1;
 
 import java.util.Observable;
 import java.util.Observer;
 
 public class MyClassA implements Observer {
 
-	Observable observable;		// 등록될 Observable
-	private boolean bPlay;		// 실행 여부
-	
-	public MyClassA(Observable o ) {
-		this.observable = o;
-		observable.addObserver(this);
-	}
-	
+    Observable observable;	// 등록될 Observable
+    private boolean bPlay;	// 실행 여부
+
+    public MyClassA(Observable o) { 
+        this.observable = o;
+        observable.addObserver(this);	// 옵저버에 현재 클래스를 등록
+    }
+
 	@Override
 	public void update(Observable o, Object arg) {
-		if (o instanceof PlayController) {
-			PlayController myControl = (PlayController) o;
-			this.bPlay = myControl.getFlag();
-			myActControl();			
-		}
-		
+        if (o instanceof PlayController) {
+            PlayController myControl = (PlayController)o;
+            this.bPlay = myControl.getFlag();
+            myActControl();
+        }
 	}
-	
-	public void myActControl() {
-		if (bPlay) {
-			System.out.println("MyClassA : 동작을 시작합니다.");
-		} else {
-			System.out.println("MyClassA : 동작을 정지합니다.");
-		}
-	}
-	
 
+    public void myActControl() {
+    	if (bPlay) {
+            System.out.println("MyClassA : 동작을 시작합니다.");
+    	} else {
+            System.out.println("MyClassA : 동작을 정지합니다.");
+    	}
+    }
 }
--- src/ObserverPattern/case1/MyClassB.java
+++ src/ObserverPattern/case1/MyClassB.java
@@ -1,36 +1,33 @@
-package ObserverPattern.case1;
+package case1;
 
 import java.util.Observable;
 import java.util.Observer;
 
 public class MyClassB implements Observer {
 
-	Observable observable;		// 등록될 Observable
-	private boolean bPlay;		// 실행 여부
-	
-	public MyClassB(Observable o ) {
-		this.observable = o;
-		observable.addObserver(this);
-	}
-	
+    Observable observable;	// 등록될 Observable
+    private boolean bPlay;	// 실행 여부
+
+    public MyClassB(Observable o) { 
+        this.observable = o;
+        observable.addObserver(this);	// 옵저버에 현재 클래스를 등록
+    }
+
 	@Override
 	public void update(Observable o, Object arg) {
-		if (o instanceof PlayController) {
-			PlayController myControl = (PlayController) o;
-			this.bPlay = myControl.getFlag();
-			myActControl();			
-		}
-		
+        if (o instanceof PlayController) {
+            PlayController myControl = (PlayController)o;
+            this.bPlay = myControl.getFlag();
+            myActControl();
+        }
 	}
-	
-	public void myActControl() {
-		if (bPlay) {
-			System.out.println("MyClassA : 동작을 시작합니다.");
-		} else {
-			System.out.println("MyClassA : 동작을 정지합니다.");
-			observable.deleteObserver(this);
-		}
-	}
-	
 
+    public void myActControl() {
+    	if (bPlay) {
+            System.out.println("MyClassB : 동작을 시작합니다.");
+    	} else {
+            System.out.println("MyClassB : 동작을 정지합니다.");
+            observable.deleteObserver(this);
+    	}
+    }
 }
